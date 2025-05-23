2f5226c6544227c5a69f8a000b9af15bb9ffb4b0
Rafael <r@bleesoft.com>
2025-05-23 13:07:56 -0300

    update git user signing key path

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -33,7 +33,7 @@
       commit.gpgSign = true;
       gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-      user.signingKey = "~/.ssh/com_bleesoft_r__dev_id";
+      user.signingKey = "~/.ssh/com_bleesoft_r__signingkey";
       core = {
         editor = "nvim";
         compression = -1;
